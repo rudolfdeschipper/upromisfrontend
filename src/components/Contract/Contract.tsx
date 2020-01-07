d6b712eb13eafe5277110c0f334e61d8a6285db5
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2020-01-07 22:54:05 +0100

    extracted loaddata method

--- src/components/Contract/Contract.tsx
+++ src/components/Contract/Contract.tsx
@@ -2,12 +2,12 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import "react-table/react-table.css";
-import {Utils} from '../Utils';
+import { Utils } from '../Utils';
 import { Link } from 'react-router-dom';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import ContractForm from './ContractForm';
-import {IContractData, IPayment} from './ContractTypes';
-import {IListState, ILoadResult} from '../GeneralTypes';
+import { IContractData, IPayment } from './ContractTypes';
+import { IListState, ILoadResult } from '../GeneralTypes';
 
 class Contract extends Component<{}, IListState<IContractData>> {
     static displayName = Contract.name;
@@ -56,11 +56,11 @@ class Contract extends Component<{}, IListState<IContractData>> {
         )
     }
 
-    private openDeleteModal = (row : { row: { _index: number}}) => {
+    private openDeleteModal = (row: { row: { _index: number } }) => {
         this.setState(
             {
                 modalDeleteIsOpen: true,
-                currentRecord: {...this.state.data[row.row._index], modifier: "Deleted" }
+                currentRecord: { ...this.state.data[row.row._index], modifier: "Deleted" }
             });
     }
 
@@ -71,6 +71,41 @@ class Contract extends Component<{}, IListState<IContractData>> {
                 currentRecord: null
             }
         )
+    }
+
+    private loadData = (state: any, instance: any) => {
+        // show the loading overlay
+        this.setState({ loading: true })
+        // fetch your data
+
+        fetch('https://localhost:5001/api/home/getcontractdata', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+                page: state.page,
+                pageSize: state.pageSize,
+                sorted: state.sorted,
+                filtered: state.filtered
+            })
+        })
+            .then(response => {
+                return response.json() as Promise<ILoadResult<IContractData>>;
+            })
+            .then(res => {
+                // Update react-table
+                this.setState({
+                    data: res.data,
+                    pages: res.pages,
+                    currentFilter: state.filtered,
+                    currentSort: state.sorted,
+                    message: res.message ? res.message : this.state.message
+                })
+            })
+            .catch(e => console.error(e))
+            .finally(() => this.setState({ loading: false }))
     }
 
     downloadToExcel = () => {
@@ -109,7 +144,7 @@ class Contract extends Component<{}, IListState<IContractData>> {
                 Header: 'Actions',
                 Cell: (row: { row: { id: any, _index: number; }; }) => (
                     <div className="w3-bar">
-                        <Link to={"/contractdetails/" + row.row.id }  className="w3-bar-item w3-button" title="Edit">
+                        <Link to={"/contractdetails/" + row.row.id} className="w3-bar-item w3-button" title="Edit">
                             <i className="fa fa-pencil" ></i>
                         </Link>
                         <button onClick={() => { this.openDeleteModal(row); }} className="w3-bar-item w3-button" title="Delete" >
@@ -164,7 +199,7 @@ class Contract extends Component<{}, IListState<IContractData>> {
                 sortMethod: Utils.dateSorter
             }
         ];
-      
+
 
         return (
             <div className="upromisContent">
@@ -178,40 +213,7 @@ class Contract extends Component<{}, IListState<IContractData>> {
                     manual
                     filterable
                     minRows={1}
-                    onFetchData={(state, instance) => {
-                        // show the loading overlay
-                        this.setState({ loading: true })
-                        // fetch your data
-
-                        fetch('https://localhost:5001/api/home/getcontractdata', {
-                            method: 'POST',
-                            headers: {
-                                'Accept': 'application/json, text/plain, */*',
-                                'Content-Type': 'application/json;charset=UTF-8'
-                            },
-                            body: JSON.stringify({
-                                page: state.page,
-                                pageSize: state.pageSize,
-                                sorted: state.sorted,
-                                filtered: state.filtered
-                            })
-                        })
-                            .then(response => {
-                                return response.json() as Promise<ILoadResult<IContractData>>;
-                            })
-                            .then(res => {
-                                // Update react-table
-                                this.setState({
-                                    data: res.data,
-                                    pages: res.pages,
-                                    currentFilter: state.filtered,
-                                    currentSort: state.sorted,
-                                    message: res.message ? res.message : this.state.message
-                                })
-                            })
-                            .catch(e => console.error(e))
-                            .finally( () => this.setState({ loading: false }))
-                    }}
+                    onFetchData={this.loadData}
                     columns={columns}
                 />
                 <p />
@@ -221,7 +223,7 @@ class Contract extends Component<{}, IListState<IContractData>> {
                 <Modal isOpen={this.state.modalAddIsOpen} >
                     <ModalHeader toggle={this.closeAddModalNoSave} charCode="&times;" >Add Contract</ModalHeader>
                     <ModalBody>
-                        <ContractForm buttonText="Add" currentData={this.state.currentRecord as IContractData}  />
+                        <ContractForm buttonText="Add" currentData={this.state.currentRecord as IContractData} />
                     </ModalBody>
                 </Modal>
                 <Modal isOpen={this.state.modalDeleteIsOpen} >
