40ceae881c9513f7e2be6e3337482331844c00ee
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2020-10-24 19:16:40 +0200

    recommit

--- generation/template/runZeroCode.bat
+++ generation/template/runZeroCode.bat
@@ -1,1 +1,5 @@
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2" --inputfile ..\model\CRUDModel.zc --template .\MainTemplate.ct
+"C:\Users\pista\source\repos\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile ..\model\CRUDModel.zc --template .\MainFrontendTemplate.ct --outputpath C:\Users\pista\source\repos\upromisfrontend\upromisfrontend\src
+
+"C:\Users\pista\source\repos\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile ..\model\CRUDModel.zc --template .\MainBackendTemplate.ct --outputpath C:\Users\pista\source\repos\upromisapi\upromisapi
+
+rem "D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2" --inputfile ..\model\CRUDModel.zc --template .\MainTemplate.ct
