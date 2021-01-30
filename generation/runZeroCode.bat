06378d3d4bec9ccbc9cdfded8f7226ff082628a9
Rudolf de Schipper <rudolf.deschipper@hotmail.com>
2021-01-30 16:48:42 +0100

    apply change to rename projects

--- generation/runZeroCode.bat
+++ generation/runZeroCode.bat
@@ -1,3 +1,3 @@
 "..\..\..\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\src --nowaitatexit
 
-"..\..\..\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\..\..\upromisapi\upromisapi
+"..\..\..\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2.exe"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\..\..\upromisapi\uPromis.Microservice.ContractAPI
--- generation/runZeroCodeBB.bat
+++ generation/runZeroCodeBB.bat
@@ -1,3 +1,3 @@
 "D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainFrontendTemplate.ct --outputpath ..\src --nowaitatexit
 
-"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\..\..\upromisapi\upromisapi
+"D:\Documents\Visual Studio 2015\Projects\ZeroCode2\ZeroCode2\bin\Release\ZeroCode2"  --loglevel 2 --inputfile .\model\CRUDModel.zc --template .\template\MainBackendTemplate.ct --outputpath ..\..\..\upromisapi\uPromis.Microservice.ContractAPI
